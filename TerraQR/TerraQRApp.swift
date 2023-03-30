6f83ce8a684b7ed0e651cc9373806ea14b997f3f
Dan Taeyoung <dan@dantaeyoung.com>
2023-03-29 20:37:39 -0400

    Initial Commit

---
+++ TerraQR/TerraQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerraQRApp.swift
+//  TerraQR
+//
+//  Created by Dan Taeyoung on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TerraQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
